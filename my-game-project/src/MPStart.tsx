547a219611ab5a55f389644c0c2be24e3c4191e3
Maximova Snezhana <kleprer@gmail.com>
2025-03-10 01:47:26 +0300

    fixed initial regime setting

--- my-game-project/src/MPStart.tsx
+++ my-game-project/src/MPStart.tsx
@@ -1,17 +1,31 @@
-import { useRef, useEffect, useState } from 'react'
-import './App.css'
-import Webcam from 'react-webcam'
-import { Holistic, POSE_LANDMARKS, HAND_CONNECTIONS, POSE_CONNECTIONS } from '@mediapipe/holistic'
-import { drawConnectors, drawLandmarks } from '@mediapipe/drawing_utils'
-import { Camera } from '@mediapipe/camera_utils'
-import Button from './Button'
+import { useRef, useEffect, useState } from "react"
+import "./App.css"
+import Webcam from "react-webcam"
+import { Holistic, POSE_LANDMARKS, HAND_CONNECTIONS, POSE_CONNECTIONS } from "@mediapipe/holistic"
+import { drawConnectors, drawLandmarks } from "@mediapipe/drawing_utils"
+import { Camera } from "@mediapipe/camera_utils"
+import Button from "./Button"
 
 const MPStart = () => {
   const webcamRef = useRef<Webcam>(null)
   const canvasRef = useRef<HTMLCanvasElement>(null)
-  const [leftHandCoords, setLeftHandCoords] = useState([]);
-  const [rightHandCoords, setRightHandCoords] = useState([]);
-  const [regime, setRegime] = useState("");
+  const leftHandCoords = useRef([]);
+  const rightHandCoords = useRef([]);
+  const regime = useRef("");
+
+  useEffect(() => {
+
+  }, [leftHandCoords])
+
+  useEffect(() => {
+
+  }, [rightHandCoords])
+
+  useEffect(() => {
+
+  }, [regime])
+
+
 
   useEffect(() => {
     const holistic = new Holistic({
@@ -47,10 +61,11 @@ const MPStart = () => {
 
   const onResults = (results: any) => {
 
-    
+    leftHandCoords.current = [];
+    rightHandCoords.current = [];
     
     if (!canvasRef.current) return
-    const canvasCtx = canvasRef.current.getContext('2d')
+    const canvasCtx = canvasRef.current.getContext("2d")
     if (!canvasCtx) return
       canvasCtx.save()
       canvasCtx.clearRect(0, 0, canvasRef.current.width, canvasRef.current.height)
@@ -60,68 +75,77 @@ const MPStart = () => {
     if (results.poseLandmarks) {
         // Drawing hand landmarks
         
-      drawConnectors(canvasCtx, results.leftHandLandmarks, HAND_CONNECTIONS, {color: 'white', lineWidth: 2});
-      drawLandmarks(canvasCtx, results.leftHandLandmarks, {color: 'white', fillColor: 'rgb(255,138,0)', lineWidth: 2, radius: 3});
-      drawConnectors(canvasCtx, results.rightHandLandmarks, HAND_CONNECTIONS,{color: 'white', lineWidth: 2});
-      drawLandmarks(canvasCtx, results.rightHandLandmarks, {color: 'white', fillColor: 'rgb(255,138,0)', lineWidth: 2, radius: 3});
+      drawConnectors(canvasCtx, results.leftHandLandmarks, HAND_CONNECTIONS, {color: "white", lineWidth: 2});
+      drawLandmarks(canvasCtx, results.leftHandLandmarks, {color: "white", fillColor: "rgb(255,138,0)", lineWidth: 2, radius: 3});
+      drawConnectors(canvasCtx, results.rightHandLandmarks, HAND_CONNECTIONS,{color: "white", lineWidth: 2});
+      drawLandmarks(canvasCtx, results.rightHandLandmarks, {color: "white", fillColor: "rgb(255,138,0)", lineWidth: 2, radius: 3});
       
       
       if (leftHandLandmarks)  {
-        setLeftHandCoords(leftHandLandmarks.map((landmark: any) => ({
+        leftHandCoords.current = leftHandLandmarks.map((landmark: any) => ({
         x: landmark.x,
         y: landmark.y
-      })));
-      console.log('Left Hand Coordinates:', leftHandCoords);
+      }));
+      console.log("Left Hand Coordinates:", leftHandCoords);
+      console.log("Right Hand Coordinates:", rightHandCoords);
+      console.log("A");
       }
       
       if (rightHandLandmarks)  {
-        setRightHandCoords(rightHandLandmarks.map((landmark: any) => ({
+        rightHandCoords.current = rightHandLandmarks.map((landmark: any) => ({
         x: landmark.x,
         y: landmark.y
-      })));
-      console.log('Right Hand Coordinates:', rightHandCoords);
+      }));
+      console.log("Right Hand Coordinates:", rightHandCoords);
+      console.log("Left Hand Coordinates:", leftHandCoords);
+      console.log("A");
       }
-      // checking coords for lefthand choosing a regime
-      if (regime == '' && rightHandCoords && !leftHandCoords) {
-        if (rightHandCoords[8]['x'] <= 0.95 && 0.65 <= rightHandCoords[8]['x']
-              && rightHandCoords[8]['y'] <= 0.7 && 0.65 <= rightHandCoords[8]['y']
-          )
-          {
-            setRegime('Обучение');
-          }
-          
-
-        if (rightHandCoords[8]['x'] <= 0.35 && 0.05 <= rightHandCoords[8]['x']
-          && rightHandCoords[8]['y'] <= 0.7 && 0.65 <= rightHandCoords[8]['y']
-          ) {
-            setRegime('Тестирование');
-          }
-    }
       
-      // checking coords for righthand choosing a regime
-      if (regime == '' && leftHandCoords && !rightHandCoords) {
-        if (leftHandCoords[8]['x'] <= 0.95 && 0.65 <= leftHandCoords[8]['x']
-            && leftHandCoords[8]['y'] <= 0.7 && 0.65 <= leftHandCoords[8]['y']
-        ) {
-            setRegime('Обучение');
-        }
-        
-
-        if (
-          leftHandCoords[8]['x'] <= 0.35 && 0.05 <= leftHandCoords[8]['x']
-              && leftHandCoords[8]['y'] <= 0.7 && 0.65 <= leftHandCoords[8]['y']
-        ) {
-            setRegime('Тестирование');
-        }
-      }
 
       console.log(regime);
     
     }
     
+    // checking coords for lefthand choosing a regime
+    if (regime.current == "" && rightHandCoords.current.length == 21 && leftHandCoords.current.length == 0) {
+      if (rightHandCoords.current[8]["x"] <= 0.95 && 0.65 <= rightHandCoords.current[8]["x"]
+            && rightHandCoords.current[8]["y"] <= 0.7 && 0.65 <= rightHandCoords.current[8]["y"]
+        )
+        {
+          console.log('обучение');
+          regime.current = "Обучение";
+        }
+        
+
+      if (rightHandCoords.current[8]["x"] <= 0.35 && 0.05 <= rightHandCoords.current[8]["x"]
+        && rightHandCoords.current[8]["y"] <= 0.7 && 0.65 <= rightHandCoords.current[8]["y"]
+        ) {
+          console.log('тест');
+          regime.current = "Тестирование";
+        }
+  }
     
+    // checking coords for righthand choosing a regime
+    if (regime.current == "" && leftHandCoords.current.length == 21 && rightHandCoords.current.length == 0) {
+      if (leftHandCoords.current[8]["x"] <= 0.95 && 0.65 <= leftHandCoords.current[8]["x"]
+          && leftHandCoords.current[8]["y"] <= 0.7 && 0.65 <= leftHandCoords.current[8]["y"]
+      ) {
+          console.log('обучение');
+          regime.current = "Обучение";
+      }
+      
+
+      if (
+        leftHandCoords.current[8]["x"] <= 0.35 && 0.05 <= leftHandCoords.current[8]["x"]
+            && leftHandCoords.current[8]["y"] <= 0.7 && 0.65 <= leftHandCoords.current[8]["y"]
+      ) {
+          console.log('тест');
+          regime.current = "Тестирование";
+      }
+    }
     
-    canvasCtx.restore()
+    canvasCtx.restore();
+    
   }
 
 
@@ -134,7 +158,7 @@ const MPStart = () => {
         <Webcam className="webcam" audio={false} mirrored ref={webcamRef} />
       </div>
       {
-        regime == '' &&
+        regime.current == "" &&
           <div className="options">
             <Button props={"Обучение"}/>
             <Button props={"Тестирование"}/>
@@ -142,11 +166,11 @@ const MPStart = () => {
         
       }
       {
-        regime == 'Обучение' &&
+        regime.current == "Обучение" &&
         <p>Обучение</p>
       }
       {
-        regime == 'Тестирование' &&
+        regime.current == "Тестирование" &&
         <p>Тестирование</p>
       }
       
